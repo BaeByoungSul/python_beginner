870ddd228c048737daf29d7151bb8d08ac83b34f
KOLONDOM\bsbae1 <bsbae1@kolon.com>
2024-12-18 15:25:11 +0900

    commit test

--- Day02/01_type.py
+++ Day02/01_type.py
@@ -1,7 +1,6 @@
 # type() 함수 사용하기
-
 print (type(3))
-print (type(33333333))
 print (type(3.3333333))
+print (type(33333333))
 
 print (type("fdsdsfa"))
--- Day02/02_number.py
+++ Day02/02_number.py
@@ -5,9 +5,9 @@ print(age, "의 타입은 ", type(age))
 score = 7.1212
 print(score, "의 타입은 ", type(score))
 
+# complex type
 polar_coordinate = 1-2j # +-를 띄워쓰지 않도록 주의
 print(polar_coordinate, "의 타입은 ", type(polar_coordinate))
-
 
 age = int(7)
 score = float(29.33)
@@ -23,3 +23,4 @@ print(age, score, polar_coordinate)
 # 함수를 통한 데이터 변환(글자 -> 숫자)
 age = int ("3484")
 print ( age, type(age))
+age2 = int ("7.0") # value error ( )
